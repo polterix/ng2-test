d6a8e31555f91f7bf9cf1f54d1d977d051b06b60
Fabien Munier <polterix@gmail.com>
2016-03-29 09:50:38 +0200

    add feature

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,21 @@
-import {Component} from 'angular2/core';
+import {Component} from 'angular2/core'
+import {TodoInput} from './todo/components/todo-input'
+import {TodoList} from './todo/components/todo-list'
+import {StatusSelector} from './todo/components/status-selector'
+import {SearchBox} from "./search/components/search-box"
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    selector: 'app',
+    directives: [TodoInput, TodoList, StatusSelector, SearchBox],
+    template: `
+    <div>
+      <search-box (update)="term = $event"></search-box>
+      <todo-input></todo-input>
+      <status-selector (select)="status = $event"></status-selector>
+      <todo-list
+      [status]="status"
+      [term]="term"
+      ></todo-list>
+    </div>`
 })
 export class AppComponent { }
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import {bootstrap}    from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
+import {TodoService} from './todo/services/todo-service';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [TodoService]);
